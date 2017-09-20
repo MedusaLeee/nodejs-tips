e4c78740cc45128917d33cd70457ec7198ddd61f
lijianxun <medusa.leee@gmail.com>
2017-09-20 16:38:42 +0800

    增加winston日志框架demo

---
+++ src/winstonDemo/default.js
@@ -0,0 +1,34 @@
+const winston = require('winston');
+
+const transportConsole = new winston.transports.Console({ json: false, timestamp: true, prettyPrint: true, colorize: true, level: 'debug' });
+const transportFileDebug = new winston.transports.File({ filename: __dirname + '/logs/debug.log', json: true, level: 'debug' });
+
+const logger = new (winston.Logger)({
+    levels: {
+        error: 0,
+        warn: 1,
+        info: 2,
+        debug: 3,
+    },
+    transports: [
+        transportConsole,
+        transportFileDebug
+    ],
+    exceptionHandlers: [
+        transportConsole,
+        transportFileDebug
+    ],
+    exitOnError: false
+});
+
+winston.addColors({
+    debug: 'blue',
+    info: 'green',
+    warn: 'yellow',
+    error: 'red'
+});
+
+logger.debug('debug message');
+logger.info('info message');
+logger.warn('warn message');
+logger.error('error message');
---
+++ src/winstonDemo/multi-transport.js
@@ -0,0 +1,168 @@
+const winston = require('winston');
+const moment = require('moment');
+
+const myLogFormatter = function (options) {
+    const timestamp = options.timestamp();
+    const level = options.level.toUpperCase();
+    const message = options.message || '';
+    let module = 'default';
+    if (options.meta && options.meta.module) {
+      module = options.meta.module;
+    }
+    const formatted = `[${timestamp}] [${level}] ${module} - `;
+    if (options.colorize) {
+      const colorStr = winston.config.colorize(options.level, formatted);
+      return `${colorStr}${message}`;
+    }
+    return `${formatted}${message}`;
+};
+
+const transportConsole = new winston.transports.Console({
+    json: false,
+    prettyPrint:true,
+    colorize: true,
+    level:'debug',
+    timestamp: function () {
+        return moment().format('YYYY-MM-DD HH:MM:ss.SSS');
+    },
+    formatter: myLogFormatter,
+});
+const transportFileDebug = new winston.transports.File({
+    name: 'full',
+    filename: __dirname + '/logs/debug.log',
+    json: true,
+    level:'debug',
+    maxsize: 1024 * 1024 * 10 // 10MB
+});
+
+const transportServiceFileDebug = new winston.transports.File({
+    name: 'service',
+    filename: __dirname + '/logs/service.log',
+    json: true,
+    level:'debug',
+    maxsize: 1024 * 1024 * 10 // 10MB
+});
+
+const transportDaoFileDebug = new winston.transports.File({
+    name: 'dao',
+    filename: __dirname + '/logs/dao.log',
+    json: true,
+    level:'debug',
+    maxsize: 1024 * 1024 * 10 // 10MB
+});
+
+winston.loggers.add('default', {
+  transports: [
+    transportConsole,
+    transportFileDebug
+  ],
+});
+
+winston.loggers.add('service', {
+    transports: [
+        transportConsole,
+        transportServiceFileDebug,
+        transportFileDebug
+    ],
+});
+
+winston.loggers.add('dao', {
+    transports: [
+        transportConsole,
+        transportDaoFileDebug,
+        transportFileDebug
+    ],
+});
+const defaultLog = winston.loggers.get('default');
+const serviceLog = winston.loggers.get('service');
+const daoLog = winston.loggers.get('dao');
+
+const getDefaultLogger = (module) => {
+  return {
+    debug: (...args) => {
+      const meta = { module };
+      const fullParams = args.concat(meta);
+      defaultLog.debug.apply(defaultLog, fullParams);
+    },
+    info: (...args) => {
+      const meta = { module };
+      const fullParams = args.concat(meta);
+      defaultLog.info.apply(defaultLog, fullParams);
+    },
+    warn: (...args) => {
+      const meta = { module };
+      const fullParams = args.concat(meta);
+      defaultLog.warn.apply(defaultLog, fullParams);
+    },
+    error: (...args) => {
+      const meta = { module };
+      const fullParams = args.concat(meta);
+      defaultLog.error.apply(defaultLog, fullParams);
+    }
+  };
+};
+
+const getServiceLogger = (module) => {
+  return {
+    debug: (...args) => {
+      const meta = { module };
+      const fullParams = args.concat(meta);
+      serviceLog.debug.apply(serviceLog, fullParams);
+    },
+    info: (...args) => {
+      const meta = { module };
+      const fullParams = args.concat(meta);
+      serviceLog.info.apply(serviceLog, fullParams);
+    },
+    warn: (...args) => {
+      const meta = { module };
+      const fullParams = args.concat(meta);
+      serviceLog.warn.apply(serviceLog, fullParams);
+    },
+    error: (...args) => {
+      const meta = { module };
+      const fullParams = args.concat(meta);
+      serviceLog.error.apply(serviceLog, fullParams);
+    }
+  };
+};
+
+const getDaoLogger = (module) => {
+  return {
+    debug: (...args) => {
+      const meta = { module };
+      const fullParams = args.concat(meta);
+      daoLog.debug.apply(daoLog, fullParams);
+    },
+    info: (...args) => {
+      const meta = { module };
+      const fullParams = args.concat(meta);
+      daoLog.info.apply(daoLog, fullParams);
+    },
+    warn: (...args) => {
+      const meta = { module };
+      const fullParams = args.concat(meta);
+      daoLog.warn.apply(daoLog, fullParams);
+    },
+    error: (...args) => {
+      const meta = { module };
+      const fullParams = args.concat(meta);
+      daoLog.error.apply(daoLog, fullParams);
+    }
+  };
+};
+
+getDefaultLogger('testDefault1').debug('a', 'b', 'c', 'd');
+getDefaultLogger('testDefault2').info('a', 'b', 'c', 'd');
+getDefaultLogger('testDefault3').warn('a', 'b', 'c', 'd');
+getDefaultLogger('testDefault4').error('a', 'b', 'c', 'd');
+
+getServiceLogger('testService1').debug('a', 'b', 'c', 'd');
+getServiceLogger('testService2').info('a', 'b', 'c', 'd');
+getServiceLogger('testService3').warn('a', 'b', 'c', 'd');
+getServiceLogger('testService4').error('a', 'b', 'c', 'd');
+
+getDaoLogger('testDao1').debug('a', 'b', 'c', 'd');
+getDaoLogger('testDao2').info('a', 'b', 'c', 'd');
+getDaoLogger('testDao3').warn('a', 'b', 'c', 'd');
+getDaoLogger('testDao4').error('a', 'b', 'c', 'd');
